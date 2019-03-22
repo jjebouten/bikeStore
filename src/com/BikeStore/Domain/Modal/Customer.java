c61ba7f7e316ba8b5f582e979fe9fa01f8a98025
Joey Bouten <joey@beech.it>
2019-03-22 13:22:42 +0100

    [TASK] Initial commit

---
+++ src/com/BikeStore/Domain/Modal/Customer.java
@@ -0,0 +1,10 @@
+package com.BikeStore.Domain.Modal;
+
+import java.util.ArrayList;
+
+public class Customer {
+    private Integer customerNumber;
+    private String firstName;
+    private String lastName;
+    private ArrayList<Integer> bikeNumbers;
+}
