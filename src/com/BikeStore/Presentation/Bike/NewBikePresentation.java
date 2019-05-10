29dfa2285be7b30eaac828a667c2554bab24c8bc
Joey Bouten <joey@beech.it>
2019-05-10 13:24:04 +0200

    [TASK] Fix error

---
+++ src/com/BikeStore/Presentation/Bike/NewBikePresentation.java
@@ -0,0 +1,15 @@
+package com.BikeStore.Presentation.Bike;
+
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+class NewBikePresentation extends BikeDefaultPresentation implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
